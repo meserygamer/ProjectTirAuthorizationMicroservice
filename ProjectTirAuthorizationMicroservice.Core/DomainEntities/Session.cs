b3386b4231d95bbbd9c23f6e80de78e9f70a635b
MeseryGG <meserygamer@gmail.com>
2024-07-19 22:43:29 +0300

    Add domain entities

---
+++ ProjectTirAuthorizationMicroservice.Core/DomainEntities/Session.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjectTirAuthorizationMicroservice.Core.DomainEntities
+{
+    public class Session
+    {
+        public Guid Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public User User { get; set; } = null!;
+    }
+}
---
+++ ProjectTirAuthorizationMicroservice.Core/DomainEntities/User.cs
@@ -0,0 +1,25 @@
+﻿namespace ProjectTirAuthorizationMicroservice.Core.DomainEntities
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Login { get; set; } = null!;
+
+        public string PasswordHash { get; set; } = null!;
+
+        public string UserName { get; set; } = null!;
+
+        public string UserSurname { get; set; } = null!;
+
+        public string UserPatronymic { get; set; } = null!;
+
+        public string UserEmail { get; set; } = null!;
+
+        public string UserPhone { get; set; } = null!;
+
+        public DateOnly UserBirtdayDate { get; set; }
+
+        public List<Session> OpenSessions { get; set; } = null!;
+    }
+}
