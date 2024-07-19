7a69a775df800320fb211c84a2fcf90ab032b18c
MeseryGG <meserygamer@gmail.com>
2024-07-19 22:52:12 +0300

    Add IMapper

---
+++ ProjectTirAuthorizationMicroservice.Database/Mappers/IMapper.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjectTirAuthorizationMicroservice.Database.Mappers
+{
+    public interface IMapper<E, D>
+    {
+        D ToDomain(E externalEntity);
+
+        E ToExternal(E domainEntity);
+    }
+}
