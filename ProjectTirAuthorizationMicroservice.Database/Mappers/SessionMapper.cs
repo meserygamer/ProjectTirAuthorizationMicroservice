52ea4fb882bed41094fa7b64a72ad08fa7d7fb83
MeseryGG <meserygamer@gmail.com>
2024-07-19 23:07:15 +0300

    Add mappers for db

---
+++ ProjectTirAuthorizationMicroservice.Database/Mappers/SessionMapper.cs
@@ -0,0 +1,30 @@
+﻿using ProjectTirAuthorizationMicroservice.Core.DomainEntities;
+using ProjectTirAuthorizationMicroservice.Database.Entities;
+
+namespace ProjectTirAuthorizationMicroservice.Database.Mappers
+{
+    public class SessionMapper : IMapper<SessionEntity, Session>
+    {
+        public Session ToDomain(SessionEntity externalEntity)
+        {
+            var domainSession = new Session
+            {
+                Id = externalEntity.Id,
+                StartDate = externalEntity.StartDate,
+                UserId = externalEntity.UserId
+            };
+            return domainSession;
+        }
+
+        public SessionEntity ToExternal(SessionEntity domainEntity)
+        {
+            var externalSession = new SessionEntity
+            {
+                Id = domainEntity.Id,
+                StartDate = domainEntity.StartDate,
+                UserId = domainEntity.UserId,
+            };
+            return externalSession;
+        }
+    }
+}
---
+++ ProjectTirAuthorizationMicroservice.Database/Mappers/UserMapper.cs
@@ -0,0 +1,42 @@
+﻿using ProjectTirAuthorizationMicroservice.Core.DomainEntities;
+using ProjectTirAuthorizationMicroservice.Database.Entities;
+
+namespace ProjectTirAuthorizationMicroservice.Database.Mappers
+{
+    public class UserMapper : IMapper<UserEntity, User>
+    {
+        public User ToDomain(UserEntity externalEntity)
+        {
+            var domainUser = new User
+            {
+                Id = externalEntity.Id,
+                Login = externalEntity.Login,
+                PasswordHash = externalEntity.PasswordHash,
+                UserName = externalEntity.UserName,
+                UserSurname = externalEntity.UserSurname,
+                UserPatronymic = externalEntity.UserPatronymic,
+                UserEmail = externalEntity.UserEmail,
+                UserPhone = externalEntity.UserPhone,
+                UserBirtdayDate = externalEntity.UserBirtdayDate,
+            };
+            return domainUser;
+        }
+
+        public UserEntity ToExternal(UserEntity domainEntity)
+        {
+            var externalUser = new UserEntity
+            {
+                Id = domainEntity.Id,
+                Login = domainEntity.Login,
+                PasswordHash = domainEntity.PasswordHash,
+                UserName = domainEntity.UserName,
+                UserSurname = domainEntity.UserSurname,
+                UserPatronymic = domainEntity.UserPatronymic,
+                UserEmail = domainEntity.UserEmail,
+                UserPhone = domainEntity.UserPhone,
+                UserBirtdayDate = domainEntity.UserBirtdayDate,
+            };
+            return externalUser;
+        }
+    }
+}
