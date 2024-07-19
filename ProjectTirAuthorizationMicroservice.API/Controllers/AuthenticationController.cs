d6da584c5f211d324cc5885f78d9017c6ea740a0
MeseryGG <meserygamer@gmail.com>
2024-07-19 12:55:01 +0300

    Project initialize

---
+++ ProjectTirAuthorizationMicroservice.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectTirAuthorizationMicroservice.Controllers
+{
+    [Route("[Controller]")]
+    public class AuthenticationController : Controller
+    {
+        
+        [HttpPost]
+        [Route("[Action]")]
+        public async Task<IActionResult> Login()
+        {
+            return Ok("Login was confirmed");
+        }
+
+        [HttpPost]
+        [Route("[Action]")]
+        public async Task<IActionResult> Logout()
+        {
+            return Ok("Logout was confirmed");
+        }
+    }
+}
