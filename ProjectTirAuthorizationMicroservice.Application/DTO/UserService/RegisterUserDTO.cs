ba88b65fbb99078a8813582b812e38919e5deb2d
MeseryGG <meserygamer@gmail.com>
2024-07-21 14:07:21 +0300

    Add validation for user

--- ProjectTirAuthorizationMicroservice.Application/DTO/UserService/RegisterUserDTO.cs
+++ ProjectTirAuthorizationMicroservice.Application/DTO/UserService/RegisterUserDTO.cs
@@ -6,10 +6,14 @@ namespace ProjectTirAuthorizationMicroservice.Application.DTO.UserService
     {
         [Required(AllowEmptyStrings = false)]
         [StringLength(30, MinimumLength = 5, ErrorMessage = "Минимальная длина логина - 5, максимальная - 30")]
-        [RegularExpression(@"^[(A - Z)(a - z)(0 - 9)]+$", ErrorMessage = "Логин должен содержать только буквы английского алфавита и цифры")]
+        [RegularExpression(@"^[(A - Z)(a - z)(0 - 9)]+$",
+            ErrorMessage = "Логин должен содержать только буквы английского алфавита и цифры")]
         public string Login { get; set; } = null!;
 
-        [Required] 
+        [Required]
+        [StringLength(30, MinimumLength = 6, ErrorMessage = "Минимальная длина пароля - 6, максимальная - 30")]
+        [RegularExpression(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[(A-Z)(a-z)(\d)(@$!%*?&)]$",
+            ErrorMessage = "Пароль должен содержать:\n хотя бы одну заглавную букву;\n хотя бы одну строчную букву;\n хотя бы одну цифру")]
         public string Password { get; set; } = null!;
 
         [Required] 
@@ -20,12 +24,14 @@ namespace ProjectTirAuthorizationMicroservice.Application.DTO.UserService
 
         public string UserPatronymic { get; set; } = null!;
 
+        [EmailAddress(ErrorMessage = "Некорректный Email")]
         public string UserEmail { get; set; } = null!;
 
-        [Required] 
+        [Required]
+        [Phone]
         public string UserPhone { get; set; } = null!;
 
-        [Required] 
+        [Required]
         public DateOnly UserBirtdayDate { get; set; }
     }
 }
